e30d4e3067dc4599619c4801daf978b7b31b6849
Won-young <dnjsdud2257@gmail.com>
2021-05-27 17:45:33 +0900

    Create member domain and repository.

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,22 @@
+package hello.hellospring.domain;
+
+public class Member {
+    private Long id; //데이터 구분을 위한 시스템 id
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    //Optional null 반환시 optional로 감싸서 반환
+    List<Member> findAll();
+}
