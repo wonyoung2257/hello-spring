b73c126376a3c6f5584347f244089abbd64dbaa7
Won-young <dnjsdud2257@gmail.com>
2021-06-23 15:22:29 +0900

    JpaRepository 추가

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity // jpa가 관리하는 엔터티
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //데이터 구분을 위한 시스템 id
+
+//    @Column(name = "username")//DB와 매핑시 사용된다.
     private String name;
 
     public Long getId() {
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,11 +5,13 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 //@Service //스프링 빈에 등록 다른 곳에서 사용가능
+@Transactional//jpa를 사용하려면 꼭 필요함
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -22,6 +24,7 @@ public class MemberService {
     /**
     * 회원 가입
     */
+
     public Long join(Member member){
         //같은 이름이 있는 중복 회원X
         //null이 아닐때 밑의 if가 동작 null일 가능성이 있으면 Optional 사용
